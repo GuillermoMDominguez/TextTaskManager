5ea8332bec7dfe722729c830fefc7ef8e2f9576d
javier <j.garoz@allgon.com>
2026-06-30 22:54:16 +0200

    Fix: state badges max-width 130px, SW network-first

--- src/tm_web/static/sw.js
+++ src/tm_web/static/sw.js
@@ -1,20 +1,17 @@
-const CACHE = 'ttm-v1';
-const PRECACHE_URLS = ['/', '/index.html', '/styles.css', '/js/app.js'];
+const CACHE = 'ttm-v2';
 
-self.addEventListener('install', event => {
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(PRECACHE_URLS))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
